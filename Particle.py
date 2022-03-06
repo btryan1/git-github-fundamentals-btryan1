df927f62e5f9faf5eb4086d3fa7169e5344e133f
btryan1 <98588452+btryan1@users.noreply.github.com>
2022-03-06 20:14:57 +0000

    Added proton bunch class

--- Particle.py
+++ Particle.py
@@ -1,16 +1,14 @@
 """This is our particle file which contains all our approximation methods and the physics used to evolve our simulation in question"""
-from re import L
 import numpy as np
 import math
-
+np.random.seed(seed=1943545)
 'Our particle class'
 class Particle():
     'Defines our values'
-    def __init__(self,position=np.array([0, 0, 0], dtype=float),velocity=np.array([0, 0, 0], dtype=float),acceleration=np.array([0, 0, 0], dtype=float),momentum=np.array([0,0,0],dtype=float),name='Particle',mass=1):
+    def __init__(self,position=np.array([0, 0, 0], dtype=float),velocity=np.array([0, 0, 0], dtype=float),acceleration=np.array([0, 0, 0], dtype=float),name='Particle',mass=1):
         self.position =  np.array(position, dtype=float)
         self.velocity =  np.array(velocity, dtype=float)
         self.acceleration = np.array(acceleration, dtype=float)
-        self.momentum = momentum
         self.mass = mass
         self.name = name
     'Returns a string which includes our data in __init__'
@@ -46,17 +44,36 @@ class Particle():
         return 0.5*self.mass*(np.linalg.norm(self.velocity))**2
     
 
-  
 class ChargedParticle(Particle):
-    def __init__(self, position=np.array([0, 0, 0], dtype=float), velocity=np.array([0, 0, 0], dtype=float), acceleration=np.array([0, 0, 0],dtype=float), momentum=np.array([0,0,0],dtype=float), name='Proton', mass=1,charge=1):
-        super().__init__(position, velocity, acceleration,momentum, name, mass)
+    def __init__(self, position=np.array([0, 0, 0], dtype=float), velocity=np.array([0, 0, 0], dtype=float), acceleration=np.array([0, 0, 0],dtype=float), name='Proton', mass=1,charge=1):
+        super().__init__(position, velocity, acceleration, name, mass)
         self.charge=charge
     
     def __repr__(self):
         return 'Charged Particle: {0}, Mass: {1:12.3e}, Charge: {2:12.3e}, Position: {3}, Velocity: {4}, Acceleration: {5}'.format(self.name,self.mass,self.charge,self.position, self.velocity,self.acceleration)
 
-class ProtonBunch(ChargedParticle):
+class ProtonBunch():
     def __init__(self):
         pass
+    def Generate_Bunch(self,proton_num):
+        Bunch=[0,0]
+        mean = [0, 0, 0]
+        mean_2=[5,5,0]
+        position_matrix = [[0.1, 0, 0], [0, 0.001, 0], [0, 0, 0.001]]
+        velocity_matrix=[[5, 0, 0], [0, 5, 0], [0, 0, 0]]
+        # using np.multinomial() method
+        proton_positions = np.random.multivariate_normal(mean, position_matrix, proton_num)
+        proton_velocities=np.random.multivariate_normal(mean_2, velocity_matrix, proton_num)
+        Bunch[0]=proton_positions
+        Bunch[1]=proton_velocities
+        return Bunch
+
+    def Averages(self,positions,velocities):
+        Average_Positions=[np.mean(positions[:,0]),np.mean(positions[:,1]),np.mean(positions[:,2])]
+        Average_Velocities=[np.mean(velocities[:,0]),np.mean(velocities[:,1]),np.mean(velocities[:,2])]
+        return(Average_Positions,Average_Velocities)
 
 
+PB=ProtonBunch()
+Bunch=PB.Generate_Bunch(2)
+print(Bunch)
