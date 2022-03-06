5bbfaefbe8401033a35d26dcc6e65f5f052bf7aa
btryan1 <98588452+btryan1@users.noreply.github.com>
2022-03-06 21:24:53 +0000

    Removed particle class instance

--- GeneralEMField2.py
+++ GeneralEMField2.py
@@ -5,15 +5,6 @@ import math
 from Particle import ChargedParticle
 import matplotlib.pyplot as plt
 
-particle = ChargedParticle(
-    position=np.array([0, 0, 0]),
-    velocity=np.array([0, 0, 0]),
-    acceleration=np.array([0, 0, 0]),
-    name="Proton",
-    mass=1.6726219E-27,
-    charge=1.602176634E-19
-)
-
 class GeneralEMField(ABC):
 
     @abstractmethod
@@ -34,9 +25,9 @@ class GeneralEMField(ABC):
         return 'EMField: E = {0}, B = {1}'.format(self.electric,self.magnetic)
 
 
-    def getAcceleration(self,velocity):
+    def getAcceleration(self,particle):
         lorentz = np.array(self.electric, dtype=float)
-        lorentz+=np.cross(velocity, self.magnetic)
+        lorentz+=np.cross(particle.velocity, self.magnetic)
         lorentz *= (particle.charge/particle.mass)
         return lorentz
     
@@ -45,14 +36,13 @@ class EMField(GeneralEMField):
     def __init__(self, ElectricField = np.array([0,0,0], dtype=float),MagneticField = np.array([0,0,0], dtype=float) ):
         super().__init__(ElectricField,MagneticField)
     
-    def TimePeriod(self):
+    def TimePeriod(self,particle):
         return (2*pi*particle.mass)/(particle.charge*np.linalg.norm(self.magnetic))
     
-    def getForce(self,momentum):
+    def getForce(self,momentum,particle):
         lorentz = np.array(self.electric, dtype=float)
         lorentz+=np.cross(particle.charge*momentum/particle.mass, self.magnetic)
         return lorentz
     
 
 time_percent=3.049968E-4    
-
